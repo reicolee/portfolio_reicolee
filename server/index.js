dcbd777e017a80d8034d6e9b224cc3cbf67026ba
Reico Lee <leereico@gmail.com>
2017-04-12 02:11:28 -0400

    contact form working

--- server/index.js
+++ server/index.js
@@ -11,6 +11,14 @@ const app = express();
 
 server.on('request', app);
 
+let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'leereico@gmail.com',
+        pass: 'andypam94361050'
+    }
+});
+
 app.use(volleyball);
 app.use(bodyParser.urlencoded({
     extended: true
@@ -19,7 +27,24 @@ app.use(express.static(path.join(__dirname, '../public')));
 app.use(favicon(path.join(__dirname, '../public', 'img', 'favicon.ico')));
 
 
+app.get('/send', function(req, res) {
+    let mailOptions = {
+        from: 'leereico@gmail.com',
+        to: 'leereico@gmail.com',
+        subject: "Portfolio Site Message from " + req.query.subject,
+        text: "Sender: "+ req.query.subject + "\n" + "Email: "+ req.query.from + "\n" + "Message: "+req.query.text
+    }
+    console.log(mailOptions);
 
+    transporter.sendMail(mailOptions, function(err, res) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log('Email sent');
+    }
+});
+
+});
 
 server.listen(process.env.PORT || 8080, function() {
     console.log('The server is listening on port 8080!');
